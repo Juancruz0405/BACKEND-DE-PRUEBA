9ec217cebd64e088ce79643f49a01ccbb4ef5446
Juancruz0405 <jgonzalezyanez4@gmail.com>
2024-01-04 15:20:43 -0300

    be2

--- src/db.js
+++ src/db.js
@@ -8,7 +8,7 @@ const VideogameModel = require("./models/Videogame");
 const GenresModel = require("./models/Genres");
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}cd`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
